06e043afd21216895705e9686a58a494fa332bca
PraiseDelia <elemdelia@gmail>
2022-02-16 23:12:48 +0100

    Started the Javascript part

---
+++ eden.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const clickCheck = document.querySelector(".__text");
+const orangeColoredSection = document.querySelector(".orange_colored_section");
+const header = document.querySelector(".__header");
+const orangeBodyContainer = document.querySelector(".orange_body_container");
+const overlay = document.querySelector(".overlay");
+const clickChpopover_mealeck = document.querySelector(".popover_meal p");
+
+
+function animate () {
+
+}
