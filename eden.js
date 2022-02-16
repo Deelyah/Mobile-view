4e440958188718fb32166662302be626172f8354
PraiseDelia <elemdelia@gmail>
2022-02-16 23:43:21 +0100

    A step forward in my animation

--- eden.js
+++ eden.js
@@ -5,7 +5,7 @@ const orangeColoredSection = document.querySelector(".orange_colored_section");
 const header = document.querySelector(".__header");
 const orangeBodyContainer = document.querySelector(".orange_body_container");
 const overlay = document.querySelector(".overlay");
-const clickChpopover_mealeck = document.querySelector(".popover_meal p");
+const popover_meal = document.querySelector(".popover_meal p");
 
 
 function animate () {
